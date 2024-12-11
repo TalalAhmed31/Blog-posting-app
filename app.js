eeccac2660fd6037081360d819156fd93c7a2dfe
Talal Ahmed <talalsidd31@gmail.com>
2024-12-12 00:23:53 +0500

    jwt authentication

--- app.js
+++ app.js
@@ -1,27 +1,53 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const session = require('express-session');
-const path = require('path');
-const mongoose = require('mongoose');
- 
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const session = require("express-session");
+const path = require("path");
+const mongoose = require("mongoose");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-const SECRET_KEY = 'your_secret_key';
+const SECRET_KEY = "TalalLearningBackendDevelopment";
 
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 
-const uri =  "mongodb://root:<replace password>@localhost:27017";
-mongoose.connect(uri,{'dbName':'SocialDB'});
+const uri = "mongodb://localhost:27017";
+mongoose.connect(uri, { dbName: "SocialDB" });
 
-const User = mongoose.model('User', { username: String, email: String, password: String });
-const Post = mongoose.model('Post', { userId: mongoose.Schema.Types.ObjectId, text: String });
+const User = mongoose.model("User", {
+  username: String,
+  email: String,
+  password: String,
+});
+const Post = mongoose.model("Post", {
+  userId: mongoose.Schema.Types.ObjectId,
+  text: String,
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(session({ secret: SECRET_KEY, resave: false, saveUninitialized: true, cookie: { secure: false } }));
-
+app.use(
+  session({
+    secret: SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 
 // Insert your authenticateJWT Function code here.
+
+function aunthenticationJWT(req, res, next) {
+  const token = req.session.token;
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
+
+  try{
+    const decoded = jwt.verify(token, SECRET_KEY);
+    req.user = decoded;
+    next();
+  } catch(error){
+    return res.status(401).json({message: 'Invalid Token'})
+  }
+}
 
 // Insert your requireAuth Function code here.
 
